7f1081ab889623b3df801a171b6e550fe57ed539
the_black_digger <94235440+saujix@users.noreply.github.com>
2023-07-28 22:30:40 +0530

    Update Spotify_Album_Downloader(SAD).py

--- Spotify_Album_Downloader(SAD).py
+++ Spotify_Album_Downloader(SAD).py
@@ -14,13 +14,13 @@ print(f'''
 
 ''')
 kini=input("Enter The Spotify Album Link \n :")
-
+folder_path=input("Where to store file? \n   Example: C:/Users/chutk/Music \n     :")
 from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import requests
-
+import os
 driver=webdriver.Chrome()
 
 driver.get(kini)
@@ -38,6 +38,8 @@ for x in url:
     url = 'https://spotifymate.com/'
     driver.get(url)
 
+    time.sleep(5)
+
     # link_enter
     email_input_locator = (By.CLASS_NAME, "form-control")
     email_input_element = driver.find_element(*email_input_locator)
@@ -45,37 +47,41 @@ for x in url:
 
     # next_click
 
+    time.sleep(5)
+
     button_locator = (By.ID, "send")
     next_button_element = driver.find_element(*button_locator)
     next_button_element.click()
 
     time.sleep(5)
 
-    x = driver.page_source
+    e = driver.page_source
 
-    soup = BeautifulSoup(x, 'lxml')
+    soup = BeautifulSoup(e, 'lxml')
     name =soup.find('div',class_='hover-underline')
     link = soup.find('a', class_='abutton is-success is-fullwidth')
 
 
 
     if link is None:
-        print("Please wait 1 minute!")
-        time.sleep(60)
+        z = x.get('href')
+        link = f"https://open.spotify.com{z}"
+
+        url = 'https://spotifymate.com/'
         driver.get(url)
 
-        # link_enter
+            # link_enter
         email_input_locator = (By.CLASS_NAME, "form-control")
         email_input_element = driver.find_element(*email_input_locator)
         email_input_element.send_keys(link)
 
-        # next_click
+            # next_click
 
         button_locator = (By.ID, "send")
         next_button_element = driver.find_element(*button_locator)
         next_button_element.click()
 
-        time.sleep(5)
+        time.sleep(10)
 
         x = driver.page_source
 
@@ -84,11 +90,27 @@ for x in url:
         link = soup.find('a', class_='abutton is-success is-fullwidth')
 
         link1 = link.get('href')
-        driver.get(link1)
+        response=requests.get(link1)
+        artist=(name.text)
+        file_path=os.path.join(folder_path,artist)
+        with open(file_path, "a") as file:
+            file.write()
+        with open(file_path,"wb") as file:
+            file.write(response.content)
+
         time.sleep(5)
         print(f"{name.text} is downloading!")
     else:
         link1 = link.get('href')
-        driver.get(link1)
+        response = requests.get(link1)
+
+        artist1 = (name.text)
+
+        artist=f"{artist1}.mp3"
+
+        file_path = os.path.join(folder_path, artist)
+
+        with open(file_path, "wb") as file:
+            file.write(response.content)
         print(f"{name.text} is downloading!")
         time.sleep(5)
